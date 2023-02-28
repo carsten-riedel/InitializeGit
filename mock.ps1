cd13679a785cd9a24f686dd031a9d4c0cf2eeeba
Carsten Riedel <carstenriedel@outlook.com>
2023-02-28 22:41:23 +0100

    ps1

---
+++ mock.ps1
@@ -0,0 +1,2 @@
+Write-Host "MOCK Press any key to continue..." -NoNewline
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
--- stub.ps1
+++ stub.ps1
@@ -1,2 +1,7 @@
-Write-Host "Press any key to continue..." -NoNewline
+Write-Host "STUB Press any key to continue..." -NoNewline
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
+
+
+Write-Host "The main script is running"
+& 'mock.ps1'
+Write-Host "The main script has finished"
