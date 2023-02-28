85fd6351198f7efeeacad659bd72a8157c438899
Carsten Riedel <carstenriedel@outlook.com>
2023-02-28 21:31:43 +0100

    echo change

--- mock.cmd
+++ mock.cmd
@@ -6,7 +6,7 @@ del /Q "%initgit%\stub.cmd"
 
 SET pgit=%LocalAppData%\PortableGit\cmd\git.exe
 echo Changes in mock.cmd downloaded from raw.githubusercontent.com take some time since there populated with the content providers. 
-echo %pgit%
+echo The git.exe is located here %pgit%
 pause
 
 
