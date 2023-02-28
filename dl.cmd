27c458d9552c4d3168e904ba33cc49340ecbadbc
Carsten Riedel <carstenriedel@outlook.com>
2023-02-28 07:24:07 +0100

    dl

---
+++ dl.cmd
@@ -0,0 +1,2 @@
+bitsadmin /transfer myDownloadJob https://github.com/git-for-windows/git/releases/download/v2.39.2.windows.1/PortableGit-2.39.2-32-bit.7z.exe C:\temp\PortableGit-2.39.2-32-bit.7z.exe
+rundll32 zipfldr.dll,RouteTheCall "myarchive.zip", "C:\Users\Username\Downloads"
