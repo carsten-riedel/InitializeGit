b06f97d12fdef70523af921e968dacc0582e8bfa
Carsten Riedel <carstenriedel@outlook.com>
2023-02-28 22:45:09 +0100

    stub

--- mock.ps1
+++ mock.ps1
@@ -1,2 +1,3 @@
 Write-Host "MOCK Press any key to continue..." -NoNewline
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
+Write-Host "`n"
--- stub.ps1
+++ stub.ps1
@@ -1,5 +1,6 @@
 Write-Host "STUB Press any key to continue..." -NoNewline
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
+Write-Host "`n"
 
 $FileName = "$env:LocalAppData\InitializeGit\mock.ps1" ; if (Test-Path $FileName) { Remove-Item $FileName } ;(new-object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/carsten-riedel/InitializeGit/main/mock.ps1","$FileName") ; &"$FileName"
 
